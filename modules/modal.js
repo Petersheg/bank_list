068ea7e9db0fc58473e489f03e8309093f62f24b
Petersheg <petersheg@gmail.com>
2021-05-24 19:19:29 +0100

    initiate sign_up function in modal.js

--- modules/modal.js
+++ modules/modal.js
@@ -4,8 +4,18 @@ const overlay = document.querySelector('.overlay');
 const loginCloseModal = document.querySelector('.login_btn--close-modal');
 const signupCloseModal = document.querySelector('.signup_btn--close-modal');
 
+// input fields
+const fullNameField = document.querySelector('.fullname');
+const mobileField = document.querySelector('.mobile');
+const bvnField = document.querySelector('.bvn');
+const amountField = document.querySelector('.amount');
+const passwordField = document.querySelector('.password');
+
+const btnRegister = document.querySelector('.register_button') 
 
 export class Modal{
+
+  #accounts = []; 
   
     constructor(btnSignupOpenModal,btnLoginOpenModal){
 
@@ -16,6 +26,7 @@ export class Modal{
       this._openModal(this.btnSignupOpenModal,signupModal);
       this._closeModal(loginCloseModal,loginModal);
       this._closeModal(signupCloseModal,signupModal);
+      this._register();
     }
   
     _openModal(btn,modal){
@@ -40,6 +51,49 @@ export class Modal{
       // Add evebt listener to overlay to close modal
       overlay.addEventListener('click',()=>{
         this.hideModal(modal);
+      })
+    }
+
+    _register(){
+      btnRegister.addEventListener('click',(e)=>{
+        e.preventDefault();
+        // Get the value fo all input fields
+        let fullName = fullNameField.value;
+        let mobile = mobileField.value;
+        let bvn = bvnField.value; 
+        let amount = amountField.value;
+        let password = passwordField.value;
+
+        // Calculate Interest rate 
+      const calInterest = function(){
+        let intRate = Date.now();
+        let newrate = Number(intRate.toString().split('').slice(11,14).join(''))/10
+        return newrate
+      }
+
+      let interest = calInterest();
+        // create an object base on the values 
+        const account = {
+          owner : fullName,
+          movements:[amount],
+          interestRate : interest,
+          password,
+          
+          mobile,
+          bvn,
+        }
+
+        // Push the object into an array
+        this.#accounts.push(account);
+
+        // Store the data inside local storage
+        //localStorage.setItem('Users', JSON.stringify(this.#accounts));
+
+        // Clear input fields
+        fullNameField.value = mobileField.value =
+        bvnField.value = amountField.value = passwordField.value = "";
+
+        console.log(this.#accounts);
       })
     }
 }
