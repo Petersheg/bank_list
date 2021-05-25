e45092d661f1b3dc1da2dfc4ed54dfa24194bf0b
Petersheg <petersheg@gmail.com>
2021-05-25 15:10:10 +0100

    fix a bug

--- modules/modal.js
+++ modules/modal.js
@@ -11,22 +11,19 @@ const bvnField = document.querySelector('.bvn');
 const amountField = document.querySelector('.amount');
 const passwordField = document.querySelector('.password');
 
-const btnRegister = document.querySelector('.register_button') 
+const btnRegister = document.querySelector('.register_form') 
 
 export class Modal{
-
-  #accounts = []; 
   
     constructor(btnSignupOpenModal,btnLoginOpenModal){
 
-        this.btnSignupOpenModal = btnSignupOpenModal;
-        this.btnLoginOpenModal = btnLoginOpenModal;
+      this.btnSignupOpenModal = btnSignupOpenModal;
+      this.btnLoginOpenModal = btnLoginOpenModal;
         
       this._openModal(this.btnLoginOpenModal,loginModal);
       this._openModal(this.btnSignupOpenModal,signupModal);
       this._closeModal(loginCloseModal,loginModal);
       this._closeModal(signupCloseModal,signupModal);
-      this._register();
     }
   
     _openModal(btn,modal){
@@ -53,47 +50,65 @@ export class Modal{
         this.hideModal(modal);
       })
     }
-
-    _register(){
-      btnRegister.addEventListener('click',(e)=>{
-        e.preventDefault();
-        // Get the value fo all input fields
-        let fullName = fullNameField.value;
-        let mobile = mobileField.value;
-        let bvn = bvnField.value; 
-        let amount = amountField.value;
-        let password = passwordField.value;
-
-        // Calculate Interest rate 
-      const calInterest = function(){
-        let intRate = Date.now();
-        let newrate = Number(intRate.toString().split('').slice(11,14).join(''))/10
-        return newrate
-      }
-
-      let interest = calInterest();
-        // create an object base on the values 
-        const account = {
-          owner : fullName,
-          movements:[amount],
-          interestRate : interest,
-          password,
-          
-          mobile,
-          bvn,
-        }
-
-        // Push the object into an array
-        this.#accounts.push(account);
-
-        // Store the data inside local storage
-        //localStorage.setItem('Users', JSON.stringify(this.#accounts));
-
-        // Clear input fields
-        fullNameField.value = mobileField.value =
-        bvnField.value = amountField.value = passwordField.value = "";
-
-        console.log(this.#accounts);
-      })
-    }
 }
+
+let accounts = [];
+class Register{
+  #user
+  constructor(){
+    this._register();
+  }
+
+  // Calculate Interest rate 
+  _calInterest(){
+    let intRate = Date.now();
+    let newrate = Number(intRate.toString().split('').slice(11,14).join(''))/10
+    return newrate
+  }
+
+  _register(){
+
+    btnRegister.addEventListener('submit',(e)=>{
+      e.preventDefault();
+      // Get the value fo all input fields
+      let fullName = fullNameField.value;
+      let mobile = mobileField.value;
+      let bvn = bvnField.value; 
+      let amount = Number(amountField.value)//Value Converted to number;
+      let password = passwordField.value;
+
+      let interest = this._calInterest();
+
+      // create an object base on the values 
+      this.#user = {
+        owner : fullName,
+        movements:[amount],
+        interestRate : interest,
+        password,
+        
+        mobile,
+        bvn,
+      }
+      console.log(this.#user);
+
+      this.setItem()
+      // Clear input fields
+      fullNameField.value = mobileField.value =
+      bvnField.value = amountField.value = passwordField.value = "";
+    })
+  }
+
+  setItem(){
+    // Push the object into an array
+    accounts.push(this.#user);
+
+    // Store the data inside local storage  
+    localStorage.setItem('Users', JSON.stringify(accounts));
+
+    if(localStorage.getItem('Users')){
+      localStorage.setItem('Users', JSON.stringify(accounts));
+    }
+  }
+}
+
+const signUp = new Register;
